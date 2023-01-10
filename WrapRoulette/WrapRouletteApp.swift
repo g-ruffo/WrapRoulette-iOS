8b3cfd6de546480a65e20e9b77434aad525d407b
g-ruffo <grayson@veltus.ca>
2023-01-09 16:00:18 -0800

    Initial Commit

---
+++ WrapRoulette/WrapRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrapRouletteApp.swift
+//  WrapRoulette
+//
+//  Created by Grayson Ruffo on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct WrapRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
