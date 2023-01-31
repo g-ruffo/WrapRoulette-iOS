94d3ca857f898e6a57de9543ed4d6649a4c685f5
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-01-31 03:30:46 -0800

    feat: Add password counter

--- WrapRoulette/Base/Views/InputEditText.swift
+++ WrapRoulette/Base/Views/InputEditText.swift
@@ -15,44 +15,54 @@ struct InputEditText: View {
     
     var isPassword: Bool = false
     var textHint: String = "Email Address"
-    var leadingIcon: String = "mail"   
+    var leadingIcon: String = "mail"
     
     var body: some View {
         ZStack(alignment: .trailing) {
-            HStack {
-                Spacer(minLength: 20)
-                HStack (alignment: .center,
-                        spacing: 15) {
-                    Image(systemName: leadingIcon)
-                        .resizable()
-                        .scaledToFit()
-                    //                    .frame(width: 30, height: 30, alignment: .center)
-                        .foregroundColor(Color("BluePrimary"))
-                        .frame(minWidth: 0, maxWidth: 30)
-                        .frame(minHeight: 0, maxHeight: 33)
-                    if isPassword {
-                        if isSecured {
-                            SecureField(textHint, text: $textInput)
+            VStack {
+                HStack {
+                    Spacer(minLength: 20)
+                    HStack (alignment: .center,
+                            spacing: 15) {
+                        Image(systemName: leadingIcon)
+                            .resizable()
+                            .scaledToFit()
+                        //                    .frame(width: 30, height: 30, alignment: .center)
+                            .foregroundColor(Color("BluePrimary"))
+                            .frame(minWidth: 0, maxWidth: 30)
+                            .frame(minHeight: 0, maxHeight: 33)
+                        if isPassword {
+                            if isSecured {
+                                SecureField(textHint, text: $textInput)
+                            } else {
+                                TextField(textHint, text: $textInput)
+                            }
+                            Button(action: {
+                                isSecured.toggle()
+                            }) {
+                                Image(systemName: self.isSecured ? "eye.slash" : "eye")
+                                    .accentColor(Color("BluePrimary"))
+                            }
+                            
                         } else {
-                            TextField(textHint, text: $textInput)
+                            TextField (textHint, text: $textInput)
+                                .font(.system(size: textFieldFontSize))
                         }
-                        Button(action: {
-                            isSecured.toggle()
-                        }) {
-                            Image(systemName: self.isSecured ? "eye.slash" : "eye")
-                                .accentColor(Color("BluePrimary"))
-                        }
-                    } else {
-                        TextField (textHint, text: $textInput)
-                            .font(.system(size: textFieldFontSize))
+                        
                     }
-                    
+                            .padding([.top,.bottom], 2)
+                            .padding(.horizontal, 5)
+                            .background(Color("LoginInputLayoutBoxColor"), alignment: .center)
+                            .cornerRadius(5)
+                    Spacer(minLength: 20)
                 }
-                        .padding([.top,.bottom], 2)
-                        .padding(.horizontal, 5)
-                        .background(Color("LoginInputLayoutBoxColor"), alignment: .center)
-                        .cornerRadius(5)
-                Spacer(minLength: 20)
+                if isPassword {
+                    Text("\(textInput.count)/24")
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 20)
+                        .font(.system(size: textFieldFontSize, weight: .bold))
+                }
             }
         }
     }
