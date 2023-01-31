bec8fbf29807e33e3ce7755d6b9b2a41388891f5
g-ruffo <grayson@veltus.ca>
2023-01-30 20:59:21 -0800

    style: Add opacity and scale variables

--- WrapRoulette/Base/View/FloatingActionButton.swift
+++ WrapRoulette/Base/View/FloatingActionButton.swift
@@ -6,21 +6,20 @@
 //
 
 import SwiftUI
-private var expandableFABSize: CGFloat = 80
-private var expandedFABItemSize: CGFloat = 55
+
+private var expandableFABRotation: CGFloat = 120
+private var disabledOpacity: Double = 0.6
+private var disabledScale: Double = 0.8
+private var shadowRadius: CGFloat = 6
+let colorEnabledFab = Color("ColorEnabledFab")
+let colorEnabledFabText = Color("ColorEnabledFabText")
+
+
 
 struct ExpandableFloatingActionButton: View {
     @State var isEnabled = true
     @State var isExpanded = false
-    
-    var buttonColor: Color {
-        isEnabled ? Color("ColorEnabledFab") : Color("ColorDisabledFab")
-    }
-    
-    var textColor: Color {
-        isEnabled ? Color("ColorEnabledFabText") : Color("ColorDisabledFabText")
-    }
-    
+
     var body: some View {
         ZStack {
             Button {
@@ -28,18 +27,22 @@ struct ExpandableFloatingActionButton: View {
                 isExpanded.toggle()
             } label: {
                 Image(systemName: "plus")
-                    .rotationEffect(.degrees(isExpanded ? 120 : 0))
+                    .rotationEffect(.degrees(isExpanded ? expandableFABRotation : 0))
                     .foregroundColor(.white)
                     .font(.system(size: 38, weight: .bold))
                     .animation(.easeInOut)
                 
             }
             .padding(24)
-            .background(buttonColor)
+            .background(colorEnabledFab)
             .mask(Circle())
-            .shadow(color: .black, radius: 10)
+            .shadow(color: isEnabled ? .black : .clear, radius: shadowRadius)
+            .opacity(isEnabled ? 1.0 : disabledOpacity)
             .zIndex(10)
-            .disabled(!isEnabled)
+            .scaleEffect(isEnabled ? 1 : disabledScale)
+                        .disabled(!isEnabled)
+            .animation(.spring(response: 0.2, dampingFraction: 0.6, blendDuration: 0.2))
+
             
             FloatingActionButtonMenuItem(isEnabled: $isEnabled, isExpanded: $isExpanded, offsety: -90)
             FloatingActionButtonMenuItem(isEnabled: $isEnabled, isExpanded: $isExpanded, offsetX: -60, offsety: -60, delay: 0.2)
@@ -49,13 +52,6 @@ struct ExpandableFloatingActionButton: View {
 }
     
 
-
-struct FloatingActionButton_Previews: PreviewProvider {
-    static var previews: some View {
-        ExpandableFloatingActionButton()
-    }
-}
-
 struct FloatingActionButtonMenuItem: View {
     @Binding var isEnabled: Bool
     @Binding var isExpanded: Bool
@@ -64,10 +60,6 @@ struct FloatingActionButtonMenuItem: View {
     var offsety: CGFloat = 0
     var delay = 0.0
 
-    var buttonColor: Color {
-        isEnabled ? Color("ColorEnabledFab") : Color("ColorDisabledFab")
-    }
-    
     var body: some View {
         Button {
             print("Show Fab menu item")
@@ -78,11 +70,52 @@ struct FloatingActionButtonMenuItem: View {
                 .font(.system(size: 26, weight: .bold))
         }
         .padding()
-        .background(buttonColor)
+        .background(colorEnabledFab)
         .mask(Circle())
         .offset(x: isExpanded ? offsetX : 0, y: isExpanded ? offsety : 0)
         .scaleEffect(isExpanded ? 1 : 0)
+        .opacity(isEnabled ? 1.0 : disabledOpacity)
+        .disabled(!isEnabled)
         .animation(.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 0).delay(delay))
+    }
+}
+
+struct FloatingActionButton: View {
+    @State var isEnabled = true
+    var icon: String = "pencil"
+
+    var body: some View {
+        ZStack {
+            Button {
+                print("Fab Clicked")
+            } label: {
+                Image(systemName: "plus")
+                    .foregroundColor(.white)
+                    .font(.system(size: 38, weight: .bold))
+            }
+            .padding(24)
+            .background(colorEnabledFab)
+            .mask(Circle())
+            .shadow(color: isEnabled ? .black : .clear, radius: shadowRadius)
+            .opacity(isEnabled ? 1.0 : disabledOpacity)
+            .zIndex(10)
+            .scaleEffect(isEnabled ? 1 : disabledScale)
+            .disabled(!isEnabled)
+            .animation(.spring(response: 0.2, dampingFraction: 0.6, blendDuration: 0.2))
+        }
+    }
+}
+
+struct FloatingActionButton_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack {
+            Spacer()
+            ExpandableFloatingActionButton()
+            Spacer()
+            FloatingActionButton()
+            Spacer()
+
+        }
     }
 }
 
