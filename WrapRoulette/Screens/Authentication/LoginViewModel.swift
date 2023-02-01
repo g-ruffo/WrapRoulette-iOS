20dd0dba76705e9b6b5c0f6d9604bbe6b8a5ac7e
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-01-31 19:44:21 -0800

    feat: Create login view model

---
+++ WrapRoulette/Screens/Authentication/LoginViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewModel.swift
+//  WrapRoulette
+//
+//  Created by Grayson Ruffo on 2023-01-31.
+//
+
+import Foundation
+
+
+class LoginViewModel: ObservableObject {
+    
+    
+    
+//    func login() {
+//        Auth.auth().signIn(withEmal: email, password: password) { result, error in
+//            if error != nil {
+//                print(error!.localizedDescription)
+//            }
+//            
+//        }
+//    }
+}
