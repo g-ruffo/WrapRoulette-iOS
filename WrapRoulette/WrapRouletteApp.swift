04c6f59808fcf5a75f53c0e6abdb756f0a5b45e3
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-03-22 16:17:54 -0700

    refactor: Update Firebase initialization code

--- WrapRoulette/WrapRouletteApp.swift
+++ WrapRoulette/WrapRouletteApp.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
-import Firebase
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
 
 @main
 struct WrapRouletteApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     @StateObject var loginViewModel = LoginViewModel()
-    
-    init() {
-        FirebaseApp.configure()
-    }
+
     var body: some Scene {
         WindowGroup {
             ContentView(viewModel: loginViewModel)
