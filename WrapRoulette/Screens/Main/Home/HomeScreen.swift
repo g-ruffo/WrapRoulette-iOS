d619f2091d514d7a2c861e1667d387ea82e548c1
Grayson Ruffo <93240608+g-ruffo@users.noreply.github.com>
2023-03-22 19:55:01 -0700

    feat: Create main home screen

---
+++ WrapRoulette/Screens/Main/Home/HomeScreen.swift
@@ -0,0 +1,22 @@
+//
+//  HomeScreen.swift
+//  WrapRoulette
+//
+//  Created by Grayson Ruffo on 2023-03-22.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeScreen()
+        }
+    }
+}
