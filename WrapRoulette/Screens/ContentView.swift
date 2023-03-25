fcb3c06026fda5a7c0efc8850dae3ba8de2ff562
Grayson Ruffo <aimesx8@gmail.com>
2023-03-24 18:12:56 -0700

    feat: Show login if user is nil or home if not

---
+++ WrapRoulette/Screens/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WrapRoulette
+//
+//  Created by Grayson Ruffo on 2023-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var viewModel: FirebaseAuthViewModel
+    
+    var body: some View {
+        Group {
+                    if viewModel.user != nil {
+                        HomeScreen()
+                    } else {
+                        LoginScreen()
+                    }
+                }.onAppear {
+                    viewModel.listenToAuthState()
+                }
+            }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
