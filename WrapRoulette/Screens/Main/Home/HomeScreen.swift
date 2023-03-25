cd44143d452da1c0c6ce2c7e56859fc1f99caf54
Grayson Ruffo <aimesx8@gmail.com>
2023-03-24 20:51:59 -0700

    feat: Add temporary sign out button

--- WrapRoulette/Screens/Main/Home/HomeScreen.swift
+++ WrapRoulette/Screens/Main/Home/HomeScreen.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct HomeScreen: View {
+    
+    @EnvironmentObject var viewModel: FirebaseAuthViewModel
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Sign Out") {
+            viewModel.signOut()
+        }
     }
 }
 
